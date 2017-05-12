71cb32e0853c18aea31eb10a25d4129fef607fd6
Jennifer Kinsey <kinseyplusplus@gmail.com>
2017-05-12 09:51:26 -0700

    adds the ability to add new brands and stores in a new row on the index.erb page.

--- app.rb
+++ app.rb
@@ -10,3 +10,24 @@ get "/" do
   @stores = Store.all
   erb :index
 end
+
+post '/add_brand' do
+  name = params['name']
+  price = params['price']
+  @brand = Brand.create(name: name, price: price)
+  if @brand.save
+    redirect '/'
+  else
+    erb(:error)
+  end
+end
+
+post '/add_store' do
+  name = params['name']
+  @store = Store.create(name: name)
+  if @store.save
+    redirect '/'
+  else
+    erb(:error)
+  end
+end
