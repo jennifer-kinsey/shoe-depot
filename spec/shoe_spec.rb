1b555ae7dc7ed1d3835ff0f02c15d9db4e74e4f5
Jennifer Kinsey <kinseyplusplus@gmail.com>
2017-05-12 09:24:11 -0700

    adds validatation testing for the Brand class including testing presence of the name and price.

--- spec/shoe_spec.rb
+++ spec/shoe_spec.rb
@@ -2,6 +2,14 @@ require "spec_helper"
 
 describe (Brand) do
   it { should have_and_belong_to_many(:stores) }
+  it("validates presence of brand name") do
+    shoe1 = Brand.new({:name => "", :price => 30})
+    expect(shoe1.save).to eq(false)
+  end
+  it("validates presence of brand shoe price") do
+    shoe1 = Brand.new({:name => "Doc Marten", :price=> nil})
+    expect(shoe1.save).to eq(false)
+  end
 end
 
 describe (Store) do
